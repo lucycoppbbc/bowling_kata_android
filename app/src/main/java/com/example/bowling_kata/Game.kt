84fce13323908f1e6e82726c548d3a510e309e41
Lucy Copp <lucyanncopp@hotmail.co.uk>
2019-12-20 13:40:39 +0000

    basic scoring system

--- app/src/main/java/com/example/bowling_kata/Game.kt
+++ app/src/main/java/com/example/bowling_kata/Game.kt
@@ -1,5 +1,23 @@
 package com.example.bowling_kata
 
 class Game {
+    private var score: Int = 0
 
+    init {
+        resetScore()
+    }
+
+
+    fun rolls(pins: Int) {
+        score += pins
+    }
+
+
+    fun score(): Int {
+        return score
+    }
+
+    fun resetScore() {
+        score = 0
+    }
 }
--- app/src/test/java/com/example/bowling_kata/GameTest.kt
+++ app/src/test/java/com/example/bowling_kata/GameTest.kt
@@ -1,5 +1,45 @@
 package com.example.bowling_kata
 
-import org.junit.Assert.*
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Test
+import org.mockito.Mock
+import kotlin.test.assert
+import kotlin.test.assertEquals
 
-class GameTest {} 
+class GameTest {
+
+
+    @Test
+    fun `score is reset on initialisation`() {
+        val game = Game()
+        assertEquals(game.score(), 0)
+    }
+
+
+    @Test
+    fun `player scores no points`() {
+        val game = Game()
+        game.rolls(0)
+        assertEquals(game.score(), 0)
+    }
+
+    @Test
+    fun `player scores one point`() {
+        val game = Game()
+        game.rolls(1)
+        assertEquals(game.score(), 1)
+    }
+
+    @Test
+    fun `player scores one point in both rounds`()  {
+        val game = Game()
+        game.rolls(1)
+        game.rolls(1)
+        assertEquals(game.score(), 2)
+    }
+
+
+
+
+}
